f3719250582e6151c8ea03b61c23c97acbee1eaa
kentshenlim <kentshen_lim24@hotmail.com>
2023-06-09 21:27:07 +0100

    Add functionality to cross button

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,12 @@
 const addBtn = document.getElementById('add-btn');
 const addForm = document.getElementById('add-form');
+const addCross = document.querySelector('.icon-close');
 
 addBtn.addEventListener('click', () => {
   addForm.classList.toggle('active');
+});
+
+addCross.addEventListener('click', function () {
+  const parent = this.parentNode;
+  parent.classList.remove('active');
 });
