3edbdff5cabc77aaf8740a0af43c13973f4231b8
kentshenlim <kentshen_lim24@hotmail.com>
2023-06-09 21:32:37 +0100

    Tidy up ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,15 @@
-const addBtn = document.getElementById('add-btn');
-const addForm = document.getElementById('add-form');
-const addCross = document.querySelector('.icon-close');
+const ui = (() => {
+  // Cache DOM
+  const addBtn = document.getElementById('add-btn');
+  const addForm = document.getElementById('add-form');
+  const addCross = document.querySelector('.icon-close');
 
-addBtn.addEventListener('click', () => {
-  addForm.classList.toggle('active');
-});
-
-addCross.addEventListener('click', function () {
-  const parent = this.parentNode;
-  parent.classList.remove('active');
-});
+  // Bind events
+  addBtn.addEventListener('click', () => {
+    addForm.classList.toggle('active');
+  });
+  addCross.addEventListener('click', function () {
+    const parent = this.parentNode;
+    parent.classList.remove('active');
+  });
+})();
