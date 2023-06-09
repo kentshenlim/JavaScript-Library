3e7ec1dcf8e361dec7584cae9e6c0ffbe8a80ce2
kentshenlim <kentshen_lim24@hotmail.com>
2023-06-09 22:02:49 +0100

    Add overlay

--- js/ui.js
+++ js/ui.js
@@ -3,13 +3,20 @@ const ui = (() => {
   const addBtn = document.getElementById('add-btn');
   const addForm = document.getElementById('add-form');
   const addCross = document.querySelector('.icon-close');
+  const overlay = document.getElementById('overlay');
 
   // Bind events
   addBtn.addEventListener('click', () => {
-    addForm.classList.toggle('active');
+    addForm.classList.add('active');
+    overlay.classList.add('active');
   });
   addCross.addEventListener('click', function () {
+    closePopup.call(this);
+  });
+
+  function closePopup() {
     const parent = this.parentNode;
     parent.classList.remove('active');
-  });
+    overlay.classList.remove('active');
+  }
 })();
