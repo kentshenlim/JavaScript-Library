5a4d247e41554afc3328a786487a94234d342632
kentshenlim <kentshen_lim24@hotmail.com>
2023-06-10 14:12:52 +0100

    Add required to form

--- js/add.js
+++ js/add.js
@@ -4,11 +4,20 @@ const add = (() => {
   const _form = document.getElementById('add-form');
   const _cross = document.querySelector('#add-form .icon-close');
   const _overlay = document.getElementById('overlay');
+  const _inputTitle = document.getElementById('input-title');
+  const _inputAuthors = document.getElementById('input-authors');
+  const _inputPageNumber = document.getElementById('input-page-number');
+  const _inputStatus = document.getElementsByName('input-status');
+  console.log(_inputTitle);
 
   // Function declaration
   function _togglePopup() {
     _form.classList.toggle('active');
     _overlay.classList.toggle('active');
+  }
+
+  function _clearForm() {
+
   }
 
   // Bind events
@@ -18,5 +27,13 @@ const add = (() => {
 
   _cross.addEventListener('click', () => {
     _togglePopup();
+  });
+
+  _form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(_form);
+
+    _togglePopup();
+    console.log(_inputStatus);
   });
 })();
