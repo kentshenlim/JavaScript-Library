3a9c33c464d9d688de68a2b940d3841ff6d3ac6e
kentshenlim <kentshen_lim24@hotmail.com>
2023-06-10 22:31:42 +0100

    Allow creating book tags in JS

--- js/ui.js
+++ js/ui.js
@@ -5,16 +5,32 @@ const ui = (() => {
   const _libraryShelf = document.getElementById('library-shelf');
 
   // Function declaration
+  function _addIcon(node, iconName) {
+    const iconNode = document.createElement('span');
+    iconNode.classList.add('icon');
+    const iconInnerNode = document.createElement('ion-icon');
+    iconInnerNode.setAttribute('name', iconName);
+    iconNode.appendChild(iconInnerNode);
+    node.insertBefore(iconNode, node.firstChild);
+  }
+
   function _renderNewBook(obj) {
     const node = document.createElement('div');
-    const title = document.createElement('p');
+    node.classList.add('book-tag-wrapper');
+    const title = document.createElement('h3');
+    title.classList.add('book-tag-title');
     const author = document.createElement('p');
+    author.classList.add('book-tag-author');
     const pageNum = document.createElement('p');
+    pageNum.classList.add('book-tag-pageNum');
     [title.textContent,
       author.textContent,
       pageNum.textContent] = [obj.inputTitleVal,
-      `by${obj.inputAuthorsVal}`,
-      `page.${obj.inputPageNumberVal}`];
+      obj.inputAuthorsVal,
+      obj.inputPageNumberVal];
+    _addIcon(title, 'book'); // Must come after textContent, else overwritten
+    _addIcon(author, 'people');
+    _addIcon(pageNum, 'bookmarks');
     node.appendChild(title);
     node.appendChild(author);
     node.appendChild(pageNum);
