7f370531d9db1502e8adb10fcd2e297808d1dcee
kentshenlim <kentshen_lim24@hotmail.com>
2023-06-11 16:44:25 +0100

    Add table for stats

--- js/stats.js
+++ js/stats.js
@@ -1,1 +1,24 @@
 import library from './library.js';
+
+const stats = (() => {
+  // Cache DOM
+  const _btn = document.getElementById('stats-btn');
+  const _form = document.getElementById('stats-form');
+  const _cross = document.querySelector('#stats-form .icon-close');
+  const _overlay = document.getElementById('overlay');
+
+  // Function declaration
+  function _togglePopup() {
+    _form.classList.toggle('active');
+    _overlay.classList.toggle('active');
+  }
+
+  // Bind events
+  _btn.addEventListener('click', () => {
+    _togglePopup();
+  });
+
+  _cross.addEventListener('click', () => {
+    _togglePopup();
+  });
+})();
